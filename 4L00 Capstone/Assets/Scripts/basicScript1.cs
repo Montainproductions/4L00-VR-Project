84f77971e3335e1c8afe1ff01ddf62141d068e8b
Matthew <mm18ef@brocku.ca>
2022-09-27 14:19:42 -0400

    Added GroundCheck
    Player can only jump if it is on a object with the Ground Layer

--- 4L00 Capstone/Assets/Scripts/basicScript1.cs
+++ 4L00 Capstone/Assets/Scripts/basicScript1.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class basicScript1 : MonoBehaviour
@@ -11,9 +12,10 @@ public class basicScript1 : MonoBehaviour
     // Set variables
     private Rigidbody rb;
     public GameObject cam;
-    public GameObject GroundCheck;
     public Boolean isGrounded;
-    
+    public LayerMask GroundLayer;
+    public float sphereRadius;
+
     // Called on first frame
     void Start()
     {
@@ -25,8 +27,8 @@ public class basicScript1 : MonoBehaviour
     void Update()
     {
         // Check for player inputs from the WASD and Space keys
-        if(Input.GetKey("d") == true){
-            Move(new Vector2(1,0));
+        if (Input.GetKey("d") == true) {
+            Move(new Vector2(1, 0));
         }
         if (Input.GetKey("a") == true)
         {
@@ -40,16 +42,21 @@ public class basicScript1 : MonoBehaviour
         {
             Move(new Vector2(0, -1));
         }
+        // Checks if Player is grounded, if yes then jumping is enabled
+        IsGrounded();
         if (Input.GetKeyDown("space") == true)
         {
-            Jump();
+            if (isGrounded == true)
+            {
+                Jump();
+            }
         }
 
         // Sets Camera movement to player's position
         float sphereX = transform.position.x;
         float sphereY = transform.position.y;
         float sphereZ = transform.position.z;
-        cam.transform.position = new Vector3(sphereX, sphereY, sphereZ-5);
+        cam.transform.position = new Vector3(sphereX, sphereY, sphereZ - 5);
     }
 
     private void Move(Vector2 direction)
@@ -60,21 +67,14 @@ public class basicScript1 : MonoBehaviour
     private void Jump()
     {
         // Add upword force
-        if(isGrounded == true)
-        {
-            rb.AddForce(new Vector3(0, 1, 0) * 5f, ForceMode.Impulse);
-        }
+        rb.AddForce(new Vector3(0, 1, 0) * 5f, ForceMode.Impulse);
+
 
     }
-    void OnTriggerEnter(Collider other)
+    // Defines a sphere around the player and checks if there are any collider's with the Ground tag instecting that sphere
+    private void IsGrounded()
     {
-        if (other.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
+        isGrounded = Physics.CheckSphere(transform.position, sphereRadius, GroundLayer);
     }
+    
 }
