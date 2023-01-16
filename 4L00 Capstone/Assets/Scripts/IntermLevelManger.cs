8f97e5c91dfbce664bf3ad86c433de12dcf6ad8b
reganoldvonhindberg <54873433+reganoldvonhindberg@users.noreply.github.com>
2023-01-16 12:55:48 -0500

    added force drop, probably
    Check out the sample scene to see if it works in VR

--- 4L00 Capstone/Assets/Scripts/IntermLevelManger.cs
+++ 4L00 Capstone/Assets/Scripts/IntermLevelManger.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class IntermLevelManger : MonoBehaviour
 {
     [SerializeField] private ShaderEditing[] listOfHighlightingObjects;
+
+    [SerializeField] private XRGrabInteractable xr;
+
+    
+    [SerializeField] private InteractionLayerMask changedInteractionLayerMask;
+    private InteractionLayerMask orignalInteractionLayerMask;
+
+    private float timer;
+    private float maxTime = 3f;
+    private bool invert;
 
     private int panicRoomState = 0;
 
     private void Start()
     {
         panicRoomState = 0;
+        orignalInteractionLayerMask = xr.interactionLayers;
+        timer = maxTime;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            if (!invert)
+            {
+                removeInteractionLayers();
+                invert = !invert;
+                timer = maxTime;
+            }
+            else
+            {
+                returnInteractionLayers();
+                invert = !invert;
+                timer = maxTime;
+            }
+            
+        }
     }
 
     public void StartObjectHighLighting()
@@ -31,5 +65,16 @@ public class IntermLevelManger : MonoBehaviour
         panicRoomState = stateNumber;
     }
 
+    public void removeInteractionLayers()
+    {
+        xr.interactionLayers = changedInteractionLayerMask;
+        Debug.Log("Interaction layer changed to nothing");
+    }
+
+    public void returnInteractionLayers()
+    {
+        xr.interactionLayers = orignalInteractionLayerMask;
+        Debug.Log("Interaction layer changed to OCDClothes");
+    }
 
 }
