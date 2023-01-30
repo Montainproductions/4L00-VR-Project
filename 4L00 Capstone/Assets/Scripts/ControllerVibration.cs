229d97bc2267e30c95ba8f5d4677f59db86fcf04
reganoldvonhindberg <mm18ef@brocku.ca>
2023-01-30 12:01:18 -0500

    Second Vibration Test

--- 4L00 Capstone/Assets/Scripts/ControllerVibration.cs
+++ 4L00 Capstone/Assets/Scripts/ControllerVibration.cs
@@ -7,14 +7,36 @@ public class ControllerVibration : MonoBehaviour
 {
 
     //[SerializeField] private XRController xrController;
-    [SerializeField] private ActionBasedController abc;
+    [SerializeField] private ActionBasedController leftController;
+    [SerializeField] private ActionBasedController rightController;
+
+    private float timer;
 
 
     private void Start()
     {
         //xrController.SendHapticImpulse(1f, 2f);
-        abc.SendHapticImpulse(1f, 2f);
+        leftController.SendHapticImpulse(1f, 2f);
+        timer = 4f;
     }
 
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Debug.Log("Start Haptics now");
+            StartControllerVibrations();
+            timer = 4f;
+        }
+    }
+
+
+    public void StartControllerVibrations()
+    {
+        leftController.SendHapticImpulse(2f, 3f);
+        rightController.SendHapticImpulse(2f, 3f);
+    }
 
 }
