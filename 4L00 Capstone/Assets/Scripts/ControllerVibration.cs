920311e99ad252f4d793eca7ff8c89f4d81d20f3
reganoldvonhindberg <mm18ef@brocku.ca>
2023-01-30 12:44:11 -0500

    Grab Input Changes

--- 4L00 Capstone/Assets/Scripts/ControllerVibration.cs
+++ 4L00 Capstone/Assets/Scripts/ControllerVibration.cs
@@ -6,37 +6,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ControllerVibration : MonoBehaviour
 {
 
-    //[SerializeField] private XRController xrController;
     [SerializeField] private ActionBasedController leftController;
     [SerializeField] private ActionBasedController rightController;
 
-    private float timer;
-
-
-    private void Start()
+    // amplitude must be between 0-1 for it to work
+    public void StartControllerVibrations(float amplitude, float duration)
     {
-        //xrController.SendHapticImpulse(1f, 2f);
-        leftController.SendHapticImpulse(1f, 2f);
-        timer = 4f;
-    }
-
-
-    private void Update()
-    {
-        timer -= Time.deltaTime;
-        if (timer < 0)
-        {
-            Debug.Log("Start Haptics now");
-            StartControllerVibrations();
-            timer = 4f;
-        }
-    }
-
-
-    public void StartControllerVibrations()
-    {
-        leftController.SendHapticImpulse(2f, 3f);
-        rightController.SendHapticImpulse(2f, 3f);
+        leftController.SendHapticImpulse(amplitude, duration);
+        rightController.SendHapticImpulse(amplitude, duration);
     }
 
 }
