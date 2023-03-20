a26e222ab58c16c79bbd0aa267292263d781b606
reganoldvonhindberg <macnemat@gmail.com>
2023-03-20 17:37:19 -0400

    Commented Schizo Code

--- 4L00 Capstone/Assets/Scripts/ManagerExtra/Levels/Sc_ScizophreniaR2.cs
+++ 4L00 Capstone/Assets/Scripts/ManagerExtra/Levels/Sc_ScizophreniaR2.cs
@@ -61,77 +61,52 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
         StartCoroutine(Phase1());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator InitalPhase()
-    {
-        yield return null;
-    }
-
+    // Phase 1 is audio sources playing
     IEnumerator Phase1()
     {
         Debug.Log("Phase1 has started");
         bool Phase1isPlaying = true;
-        AudioSource bossLineOne = null;
-        AudioSource bossLineTwo = null;
-        AudioSource bossLineThree = null;
-        AudioSource radio = null;
-
-        for (int i = 0; i < scizoRoomPhase1SudioSources.Length; i++)
-        {
-            if (scizoRoomPhase1SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.BossLine1)
-            {
-                bossLineOne = scizoRoomPhase1SudioSources[i].audioSource;
-            }
-            else if (scizoRoomPhase1SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.BossLine2)
-            {
-                bossLineTwo = scizoRoomPhase1SudioSources[i].audioSource;
-            }
-            else if (scizoRoomPhase1SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.BossLine3)
-            {
-                bossLineThree = scizoRoomPhase1SudioSources[i].audioSource;
-            }
-            else if (scizoRoomPhase1SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.Radio)
-            {
-                radio = scizoRoomPhase1SudioSources[i].audioSource;
-            }
-        }
 
         while(Phase1isPlaying)
         {
+            // Go through each audio clip in the Phase1 array
+            // The will play in order of the Array and wait until the last clip has finished before starting the next
             for (int i = 0; i < scizoRoomPhase1SudioSources.Length; i++)
             {
+                // Set the audio clip for the audio source
                 scizoRoomPhase1SudioSources[i].audioSource.clip = scizoRoomPhase1SudioSources[i].audioClip;
                 scizoRoomPhase1SudioSources[i].audioSource.Play();
 
                 Debug.Log(scizoRoomPhase1SudioSources[i].audioSource.name + " is playing");
 
+                // If the audio clip is the radio then contiune immediately to the next clip
                 if (scizoRoomPhase1SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.Radio)
                 {
                     continue;
                 }
 
+                // Wait until the current audio is down playing before starting the next clip
                 while (scizoRoomPhase1SudioSources[i].audioSource.isPlaying)
                 {
                     yield return null;
                 }
             }
 
+            // Stop the Coroutine after going through all audio clips
             Phase1isPlaying = false;
         }
 
         Debug.Log("Phase1 has ended");
+        // End of Phase 1, move to phase 2 immediately
         StartCoroutine(Phase2());
     }
 
+    // Phase2 is Shadows moving
     IEnumerator Phase2()
     {
         Debug.Log("Phase2 has started");
         bool phase2IsPlaying = true;
+        // Initialize variables
         float counter = 0f;
         float duration = 10f;
 
@@ -139,12 +114,15 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
         {
             counter += Time.deltaTime;
 
+            // Move the group of shadows along the z axis over time
             float newPositionZ = Mathf.Lerp(5f, -5f, counter / duration);
             shadowsGroupTransform.position = new Vector3(shadowsGroupTransform.position.x, shadowsGroupTransform.position.y, newPositionZ);
 
+            // Increase the y scale of the shadows over time
             float newScaleY = Mathf.Lerp(1f, 5f, counter / duration);
             shadowsGroupTransform.localScale = new Vector3(shadowsGroupTransform.localScale.x, newScaleY, shadowsGroupTransform.localScale.z);
 
+            // Once the timer has reached the desired duration stop Phase 2
             if(counter >= duration)
             {
                 Debug.Log("Phase2 has ended");
@@ -153,11 +131,13 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
             yield return null;
         }
 
+        // Start Phase 3
         StartCoroutine(Phase3());
     }
 
     IEnumerator Phase3()
     {
+        // Old Code
         /*audioSources[0].Stop();
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange); //Slowly increase current velocity in the x direction untill max is reached
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange); //Slowly increase current velocity in the z direction untill max is reached
@@ -171,7 +151,7 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
 
         Debug.Log("Phase3 has started");
         float timer = 0f;
-        float duration = 10f;
+        //float duration = 10f;
         bool phase3IsPlaying = true;
 
         while (phase3IsPlaying)
@@ -189,11 +169,13 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
                 timer += Time.deltaTime;
                 mug.gameObject.transform.position = new Vector3(mug.gameObject.transform.position.x, mug.gameObject.transform.position.y, positionZ);*/
 
+                // Move the mug towards the given end location over time
                 mug.gameObject.transform.position = Vector3.MoveTowards(mug.gameObject.transform.position, mugLocations[0].transform.position, 0.1f * Time.deltaTime);
 
                 yield return null;
             }
 
+            // Not needed anymore, Phase 3 will last as long as it needs to
             /*if (timer >= duration)
             {
                 Debug.Log("Phase3 has ended");
@@ -211,6 +193,7 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
                 {
 
                     Debug.Log(timer);
+                    // After 1 second move the mug back to its original position
                     if (timer > 1f)
                     {
                         mug.velocity = new Vector3(0, 0, 0);
@@ -219,6 +202,7 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
                         mug.velocity = new Vector3(0, 0, 0);
                     }
 
+                    // Play Audio clips like in Phase 1
                     scizoRoomPhase3SudioSources[i].audioSource.clip = scizoRoomPhase3SudioSources[i].audioClip;
                     scizoRoomPhase3SudioSources[i].audioSource.Play();
 
@@ -235,6 +219,7 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
             }
             
         }
+        // Start Phase 4
         StartCoroutine(Phase4());
     }
 
@@ -243,10 +228,12 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
         Debug.Log("Phase4 has started");
         bool phase4IsPlaying = true;
 
+        // Activate the Fire Effects
         firePartciles.SetActive(true);
 
         while (phase4IsPlaying)
         {
+            // Just like in Phase 1 play audio sources in the order the appear in the array
             for (int i = 0; i < scizoRoomPhase3SudioSources.Length; i++)
             {
                 scizoRoomPhase4SudioSources[i].audioSource.clip = scizoRoomPhase4SudioSources[i].audioClip;
@@ -254,11 +241,13 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
 
                 Debug.Log(scizoRoomPhase4SudioSources[i].audioSource.name + " is playing");
 
+                // If the audio clip is the fire alarm contiune immediately onto the next audio clip
                 if (scizoRoomPhase4SudioSources[i].audioToBePlayed == ScizoRoomAudioData.AudioToBePlayed.FireAlarm)
                 {
                     continue;
                 }
 
+                // wait until the current audio clip is done before moving onto the next one
                 while (scizoRoomPhase4SudioSources[i].audioSource.isPlaying)
                 {
                     yield return null;
@@ -269,13 +258,16 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
         }
         yield return null;
         Debug.Log("Phase4 has ended");
+        // End of Schizo Room
     }
 
+    // Called by opening the email on the computer
     public void BeginPhaseOne()
     {
         StartCoroutine(Phase1());
     }
 
+    // Called when the mug's collider hits the ground
     public void MugHitFool()
     {
         Debug.Log("Mug has hit the floor");
@@ -286,8 +278,10 @@ public class Sc_ScizophreniaR2 : MonoBehaviour
 [System.Serializable]
 public class ScizoRoomAudioData
 {
+    // Makes it easier to load both the audio source and audio clip within the same array
     public AudioSource audioSource;
     public AudioClip audioClip;
+    // Helps define whether the audio clip needs special treatment
     public enum AudioToBePlayed
     {
         BossLine1,
