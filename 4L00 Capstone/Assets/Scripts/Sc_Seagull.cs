4c656df7c2a867657911c0f4d6b41927731614db
Joaquin De Losada <montainproductions@gmail.com>
2023-04-10 18:05:56 -0400

    Started work on seagull

--- 4L00 Capstone/Assets/Scripts/Sc_Seagull.cs
+++ 4L00 Capstone/Assets/Scripts/Sc_Seagull.cs
@@ -4,18 +4,69 @@ using UnityEngine;
 
 public class Sc_Seagull : MonoBehaviour
 {
-    private float hungerLevel, hoverTimer, currentTimer;
+    [SerializeField]
+    private Transform[] travelLocations;
+    private Transform[] chossenTravelLocations = new Transform[4];
+    private Transform travelLocation;
 
+    private int travelLocationPosition;
+
+    [SerializeField]
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        travelLocationPosition = 0;
+        StartCoroutine(ChooseTravelLocations());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(travelLocation.position, gameObject.transform.position) < 1)
+        {
+            NextPosition();
+        }
+        else
+        {
+            gameObject.transform.position += travelLocation.position * speed * Time.deltaTime;
+        }
+    }
+
+    public void NextPosition()
+    {
+        if (travelLocationPosition >= chossenTravelLocations.Length)
+        {
+            travelLocationPosition = 0;
+        }else
+        {
+            travelLocationPosition++;
+        }
+
+        travelLocation = chossenTravelLocations[travelLocationPosition];
+    }
+
+    IEnumerator ChooseTravelLocations()
+    {
+        int chosenLocationPos = 0;
+        for(int i = 0; i < travelLocations.Length; i++)
+        {
+            float locationChossen = Random.Range(0.0f, 1.0f);
+            if (chossenTravelLocations[chosenLocationPos] != null && locationChossen > 0.5f)
+            {
+                chossenTravelLocations[chosenLocationPos] = travelLocations[i];
+                chosenLocationPos++;
+            }
+        }
+        StartCoroutine(StartTimer());
+        yield return null;
+    }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(4);
+        StartCoroutine(ChooseTravelLocations());
+        yield return null;
     }
 }
