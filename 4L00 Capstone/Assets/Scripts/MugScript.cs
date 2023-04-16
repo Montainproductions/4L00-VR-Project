db58438c2518912fd006b5c35ab5181b631b5aa6
Matthew MacNeil <macnemat@gmail.com>
2023-04-16 16:29:48 -0400

    Cup falling sounds

--- 4L00 Capstone/Assets/Scripts/MugScript.cs
+++ 4L00 Capstone/Assets/Scripts/MugScript.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class MugScript : MonoBehaviour
 {
     [SerializeField] private Sc_ScizophreniaR2 levelmanager;
+    private AudioSource cupFallingAudioSource;
+
+    private void Start()
+    {
+        cupFallingAudioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            cupFallingAudioSource.Play();
             levelmanager.MugHitFool();
         }
     }
