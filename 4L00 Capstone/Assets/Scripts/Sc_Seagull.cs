f46a39917feeb89f3fa7d4e223a03c8d1a33085b
Joaquin De Losada <montainproductions@gmail.com>
2023-04-16 19:06:01 -0400

    Updated seagull

--- 4L00 Capstone/Assets/Scripts/Sc_Seagull.cs
+++ 4L00 Capstone/Assets/Scripts/Sc_Seagull.cs
@@ -6,7 +6,7 @@ public class Sc_Seagull : MonoBehaviour
 {
     [SerializeField]
     private Transform[] travelLocations;
-    private Transform[] chossenTravelLocations = new Transform[4];
+    private Transform[] chossenTravelLocations = new Transform[6];
     private Transform travelLocation;
 
     private int travelLocationPosition;
@@ -17,6 +17,7 @@ public class Sc_Seagull : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        travelLocation = null;
         travelLocationPosition = 0;
         StartCoroutine(ChooseTravelLocations());
     }
@@ -24,7 +25,9 @@ public class Sc_Seagull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(travelLocation.position, gameObject.transform.position) < 1)
+        Debug.Log(travelLocation);
+        Debug.Log(chossenTravelLocations[0]);
+        if(Vector3.Distance(travelLocation.position, gameObject.transform.position) < 0.5f)
         {
             NextPosition();
         }
@@ -53,12 +56,13 @@ public class Sc_Seagull : MonoBehaviour
         for(int i = 0; i < travelLocations.Length; i++)
         {
             float locationChossen = Random.Range(0.0f, 1.0f);
-            if (chossenTravelLocations[chosenLocationPos] != null && locationChossen > 0.5f)
+            if (chossenTravelLocations[chosenLocationPos] == null && locationChossen > 0.5f)
             {
                 chossenTravelLocations[chosenLocationPos] = travelLocations[i];
                 chosenLocationPos++;
             }
         }
+        travelLocation = chossenTravelLocations[0];
         StartCoroutine(StartTimer());
         yield return null;
     }
